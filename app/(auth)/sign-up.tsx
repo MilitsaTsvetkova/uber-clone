e951c95858ca438139ed1f7fdc87ca2a6e9d3f90
MilitsaTsvetkova <militsa88@yahoo.com>
2025-08-07 11:13:14 +0300

    signup page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,8 +1,72 @@
-import React from "react";
-import { View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
-  return <View>SignUp</View>;
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignUp = async () => {
+    // Handle sign up logic here
+    console.log("Sign Up:", form);
+  };
+
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create your account
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(text) => setForm({ ...form, name: text })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(text) => setForm({ ...form, email: text })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(text) => setForm({ ...form, password: text })}
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={handleSignUp}
+            className="mt-6"
+          />
+
+          <Link
+            href="/(auth)/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default SignUp;
